8cf3e65fb81173bbf5298a409d7e3c32582a2c36
oscarcontreras24 <62817339+oscarcontreras24@users.noreply.github.com>
2020-03-28 16:58:34 -0600

    Update data.py
    push3

--- stats/data.py
+++ stats/data.py
@@ -1,10 +1,13 @@
 import os
 import glob
-
 import pandas as pd
 
+#a = os.path.join('/Users/I522537/Documents/pluralsight-projects-Python-Baseball-83868ef/', 'games', '*.EVE')
+
+#game_files = glob.glob(os.path.join('/Users/I522537/Documents/pluralsight-projects-Python-Baseball-83868ef/', 'games', '*.EVE'))
 game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
 game_files.sort()
+#print(game_files)
 
 game_frames = []
 for game_file in game_files:
@@ -16,9 +19,14 @@ games = pd.concat(game_frames)
 games.loc[games['multi5'] == '??', ['multi5']] = ''
 
 identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
+#print(identifiers.head())
+
 identifiers = identifiers.fillna(method='ffill')
 identifiers.columns = ['game_id', 'year']
+#print(identifiers.head())
 
 games = pd.concat([games, identifiers], sort=False, axis=1)
+#print(games.head())
 games = games.fillna(' ')
+#print(pd.unique(games['type'].values.ravel()))
 games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
