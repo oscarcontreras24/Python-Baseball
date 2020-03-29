d140b2e2e6c23acfa20aabe727125d21291b9083
oscarcontreras24 <62817339+oscarcontreras24@users.noreply.github.com>
2020-03-29 15:16:35 -0600

    Update offense.py
    test push

--- stats/offense.py
+++ stats/offense.py
@@ -1,2 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from data import games
+
+plays = games[games['type'] == 'play']
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
+
+#find teh rows in the event column that contain S, D, etc.
+hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'), ['inning', 'event']]
+#print(hits.head())
+
+#converting in the inning elements from strings to numbers
+hits[:, 'inning'] = pd.to_numeric(hits.loc[:, 'inning'])
+
+replacements = {r'^S(.*)': 'single',
+                r'^D(.*)': 'double',
+                r'^T(.*)': 'triple',
+                r'^HR(.*)': 'hr'}
+
+#renaming the values for the event column
+hit_type = hits['event'].replace(replacements, regex = True)
+print(hit_type.head())
