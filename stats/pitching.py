88abd204780fb0b2365baa62f4eb007ab266a380
oscarcontreras24 <62817339+oscarcontreras24@users.noreply.github.com>
2020-03-29 14:49:34 -0600

    Update pitching.py
    push_test

--- stats/pitching.py
+++ stats/pitching.py
@@ -7,5 +7,17 @@ plays = games[games['type'] == 'play']
 #print(plays['event'].str.contains('K'))
 strike_outs = plays[plays['event'].str.contains('K')]
 #print(strikeouts.head())
+# print(strike_outs.loc[strike_outs['year'] == '1933', 'year']) == 8
+
+#counts how many data points exist for a gven gameid in a given year
 strike_outs = strike_outs.groupby(['year', 'game_id']).size()
-print(strike_outs.head())
+#print(strike_outs.head(50))
+
+strike_outs = strike_outs.reset_index(name = 'strike_outs')
+#print(strike_outs.head())
+
+# converting year and strike_out elements from strings to numbers
+strike_outs = strike_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
+
+strike_outs.plot(x = 'year', y = 'strike_outs', kind = 'scatter').legend('Strike Outs')
+plt.show()
