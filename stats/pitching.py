e01be83066313ce0f1af0bf7f5ebd2ae54cd0fe6
oscarcontreras24 <62817339+oscarcontreras24@users.noreply.github.com>
2020-03-29 14:29:05 -0600

    Update pitching.py
    push_test

--- stats/pitching.py
+++ stats/pitching.py
@@ -3,4 +3,9 @@ import matplotlib.pyplot as plt
 from data import games
 
 plays = games[games['type'] == 'play']
-print(plays.head())
+#print(plays.head())
+#print(plays['event'].str.contains('K'))
+strike_outs = plays[plays['event'].str.contains('K')]
+#print(strikeouts.head())
+strike_outs = strike_outs.groupby(['year', 'game_id']).size()
+print(strike_outs.head())
