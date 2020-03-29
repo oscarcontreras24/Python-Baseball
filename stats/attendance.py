74a95ff06ea976064b865381918d2bb91403f78f
oscarcontreras24 <62817339+oscarcontreras24@users.noreply.github.com>
2020-03-29 11:19:54 -0600

    Update attendance.py
    push1

---
+++ stats/attendance.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from data import games
+
+#print(games.head())
+attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance'), ['year', 'multi3']]
+#print(attendance.head())
+
+attendance.rename(columns = {'year': 'year', 'multi3': 'attendance'}, inplace = True)
+#print(attendance.head())
+
+attendance.loc[:, "attendance"] = pd.to_numeric(attendance.loc[:, "attendance"])
+#print(type(attendance['attendance']))
+
+attendance.plot(x = 'year', y = 'attendance', figsize = (15,7), kind = 'bar')
+plt.xlabel('Year')
+plt.ylabel('Attendance')
+plt.axhline(attendance['attendance'].mean(), label = 'meanline', linestyle = '--', color = 'green')
+plt.show()
