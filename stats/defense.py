f88dbd25464470f181de11a0b54fa2eb889e8cc3
oscarcontreras24 <62817339+oscarcontreras24@users.noreply.github.com>
2020-03-29 18:38:34 -0600

    Update defense.py
    test

--- stats/defense.py
+++ stats/defense.py
@@ -2,5 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from frames import games, info, events
 
-#plays = games.query('type == play & event != NP')
-#plays.head()
+plays = games.query("type == 'play' & event != 'NP'")
+
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
+#print(plays.head())
+
+#ensurng that consecutive player appearneces aren't duplicated but kept to one index per player
+pa = plays.loc[plays['player'].shift() != plays['player'], ['year', 'game_id', 'inning', 'team', 'player']]
+#print(pa.head())
+
+pa = pa.groupby(['year', 'game_id', 'team']).size().reset_index(name = 'PA')
+
+#print(events.head())
+
+#makes the columns origianlly asigned to the data frame the indexes for the data frame
+events = events.set_index(['year', 'game_id', 'team', 'event_type'])
+#print(events.head())
+
+#making a data frame that takes the values of a multi-index and makes the element in a column, into a column and making each row indexed like before
+events = events.unstack().fillna(0).reset_index()
+
+events.columns = events.columns.droplevel()
+#print(events.head())
+
+events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE', 'SO']
+events = events.rename_axis('None')
+print(events.head())
